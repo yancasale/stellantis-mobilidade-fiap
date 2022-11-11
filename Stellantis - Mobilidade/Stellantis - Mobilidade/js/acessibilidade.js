beb6b481bb9092e26c49f8b71f99100f6d613178
Yan Casale <79453924+yancasale@users.noreply.github.com>
2022-11-10 23:11:29 -0300

    Add files via upload

---
+++ Stellantis - Mobilidade/Stellantis - Mobilidade/js/acessibilidade.js
@@ -0,0 +1,15 @@
+$(document).on('click', '.acessibilidadeMenu__item.bold', function (event) {
+    $(this).toggleClass('active');
+    $('body').toggleClass('bodyBold');
+  });
+  
+  $(document).on('click', '.acessibilidadeMenu__item.largerFont', function (event) {
+    $(this).toggleClass('active');
+    $('body').toggleClass('largerFont');
+  });
+  
+  
+  $(document).on('click', '.acessibilidadeMenu__item.contraste', function (event) {
+    $(this).toggleClass('active');
+    $('body').toggleClass('contraste');
+  });
